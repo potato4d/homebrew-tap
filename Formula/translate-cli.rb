95db5b7d6f8fa6392f5a617a9b38d3508c8342da
Takuma HANATANI <mail@potato4d.me>
2026-05-29 12:01:51 +0900

    Update translate-cli formula to v0.1.5

--- Formula/translate-cli.rb
+++ Formula/translate-cli.rb
@@ -4,28 +4,28 @@
 class TranslateCli < Formula
   desc "Translate text through local Agent CLIs"
   homepage "https://github.com/potato4d/translate-cli"
-  version "0.1.4"
+  version "0.1.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/potato4d/homebrew-tap/releases/download/translate-cli-v0.1.4/t-darwin-amd64.tar.gz"
-      sha256 "5789b9f7417833d59c5c1dfb2393b0bf9e776c939d3f4fed4f940810defffb56"
+      url "https://github.com/potato4d/homebrew-tap/releases/download/translate-cli-v0.1.5/t-darwin-amd64.tar.gz"
+      sha256 "bbe23f9ad514763d77591b4abcf9577ba0a9b6636df5253f53c299888b229fbb"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/potato4d/homebrew-tap/releases/download/translate-cli-v0.1.4/t-darwin-arm64.tar.gz"
-      sha256 "7fbb75dc2967aa96717b20a54591143bfe75437413de35d6b7fee28d2342165f"
+      url "https://github.com/potato4d/homebrew-tap/releases/download/translate-cli-v0.1.5/t-darwin-arm64.tar.gz"
+      sha256 "0febf0a1b662bb045332ef1d189976e6f03962cdf8d45e6c272ac233b6195ac2"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/potato4d/homebrew-tap/releases/download/translate-cli-v0.1.4/t-linux-amd64.tar.gz"
-      sha256 "fc3760050b199737b4df7981e0121827c86a3280fa746f85b8ea40bd2e3a2132"
+      url "https://github.com/potato4d/homebrew-tap/releases/download/translate-cli-v0.1.5/t-linux-amd64.tar.gz"
+      sha256 "aefa76a15fecf318630cf60c8b73c15423bb1126ddcc15c50cac172ac2c2e853"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/potato4d/homebrew-tap/releases/download/translate-cli-v0.1.4/t-linux-arm64.tar.gz"
-      sha256 "ec4762623ff299d9f99ff13c6d571205148785e71cfaabb815eeaff863eeeb69"
+      url "https://github.com/potato4d/homebrew-tap/releases/download/translate-cli-v0.1.5/t-linux-arm64.tar.gz"
+      sha256 "7ff96e29a4f4e251df19820c6b8060a179dc4c3eee78de7243e0b388ec1fa094"
     end
   end
 
